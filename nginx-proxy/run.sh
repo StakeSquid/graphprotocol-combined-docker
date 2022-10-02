7fa77f70a2654c9b005f24b2f0db5578f3154819
trader-payne <baboialex95@gmail.com>
2022-10-03 00:04:42 +0300

    fix ssl for good

--- nginx-proxy/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /etc/nginx/htpasswd/
-echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_MAINNET
-echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_TESTNET
-nginx -g "daemon off;"
