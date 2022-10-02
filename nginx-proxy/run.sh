30adc23a9f72e1fb740495c232fc9d3489c5fcb4
trader-payne <baboialex95@gmail.com>
2022-10-02 22:59:56 +0300

    try fix nginx again

--- nginx-proxy/run.sh
+++ nginx-proxy/run.sh
@@ -2,6 +2,6 @@
 
 rm -rf /etc/nginx/htpasswd/
 mkdir /etc/nginx/htpasswd/
-echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_MAINNET
-echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_TESTNET
+echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -salt password $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_MAINNET
+echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -salt password $HTTP_AUTH_PASSWORD) > /etc/nginx/htpasswd/$PROMETHEUS_HOST_TESTNET
 nginx -g "daemon off;"
